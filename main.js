978534ee8d821d63eec27c4058089a89635de567
praneil231 <84107249+praneil231@users.noreply.github.com>
2022-02-24 18:46:36 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
 function startClassification (){
 navigator.mediaDevices.getUserMedia({audio: true});
-startClassifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/n2dB9kVqh"
-    )
+classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/n2dB9kVqh/model.json",modelReady);
+
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults (error, results){
+ if (error) {
+     console.error(error);
+ }   
+ else{
+     console.log(results);
+     random_number_r=Math.floor(Math.random() * 255) + 1;
+     random_number_g=Math.floor(Math.random() * 255) + 1;
+     random_number_b=Math.floor(Math.random() * 255) + 1;
+     document.getElementById("result_label").innerHTML="i can hear - "+
+     results[0].label;
+     document.getElementById("result_confidence").innerHTML="Accuracy - "+
+     (results[0].confidence*100).toFixed(2)+" %";
+     document.getElementById("result_label").style.color="rgb("
+     +random_number_r+","+random_number_g+","+random_number_b+")";
+     document.getElementById("result_confidence").style.color="rgb("
+     +random_number_g+","+random_number_b+","+random_number_r+")";
+     img1=document.getElementById("alien1");
+     img2=document.getElementById("alien2");
+     img3=document.getElementById("alien3");
+     img4=document.getElementById("alien4");
+     if (results[0].label == "clap") {
+         img1.src="aliens-01.gif";
+         img2.src="aliens-02.png";
+         img3.src="aliens-03.png";
+         img4.src="aliens-04.png";
+     }
+     if (results[0].label == "abacus") {
+        img1.src="aliens-01.png";
+        img2.src="aliens-02.gif";
+        img3.src="aliens-03.png";
+        img4.src="aliens-04.png";
+
+    }
+    if (results[0].label == "magnet") {
+        img1.src="aliens-01.png";
+        img2.src="aliens-02.png";
+        img3.src="aliens-03.gif";
+        img4.src="aliens-04.png";
+        
+
+    }
+    if (results[0].label == "Background Noise") {
+        img1.src="aliens-01.png";
+        img2.src="aliens-02.png";
+        img3.src="aliens-03.png";
+        img4.src="aliens-04.gif";
+    } 
+ }
 }
